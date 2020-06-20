d20486807863562b9cd51742bb73292e7a5c47ee
Richard <richdenys_dc@hotmail.com>
2020-06-20 16:35:19 -0500

    proyectocige_0.0.1

--- src/Footer/Estadis.svelte
+++
@@ -1,7 +0,0 @@
-<script>
-let foto = "./src/img/assets/wave.svg";
-</script>
-
-<div class="w-full h-auto ">
-<img  src={foto} alt="">
-</div>
--- src/Title.svelte
+++
@@ -1,9 +0,0 @@
-<script>
-    export let title="";
-</script>
-<section>
-    <h2 class="text-5xl text-white transform translate-x-24">
-    {title}
-    </h2>
-    <img src="./src/img/assets/title.svg" alt="">
-</section>
